79e66388e546724b071c80b84ec1a39cc78dfee7
polchinski <165171058+hackematrix@users.noreply.github.com>
2025-06-18 14:30:14 +0800

    Update main.py

--- 脚本/调用genimi/main.py
+++ 脚本/调用genimi/main.py
@@ -1,14 +1,23 @@
 from google import genai
 
-with open("api_key.txt","r") as f:
-    data=f.read().strip()
+def api_key():
+    try:
+        with open('api_key.txt','r') as f:
+            data=f.read().strip()
+        return data
+    except FileNotFoundError:
+        print("can't find file")
+        return None     
 
-client=genai.Client(api_key=data)
+def main():
+    client=genai.Client(api_key=api_key())
+    response=client.models.generate_content(
+        model='gemini-2.5-flash',
+        contents='如何解释欧拉公式'
+    )
+    print(response.text)
 
-response=client.models.generate_content(
-    model="gemini-2.0-flash",
-    contents="用中文解释麦克斯韦方程组"
-)
 
-print(response.text)
 
+if __name__ == "__main__":
+    main()
